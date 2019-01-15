9d86e114453036f3735a348bdffc17738c94fdc2
Dominik <d.wisskirchen@gmail.com>
2019-01-15 11:44:35 +0100

    intial commit

---
+++ update_website.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+OUTDIR=.
+LOCAL_GHOST="http://localhost:2368"
+GITHUB_PAGES_URL="http://twoandahalfvan.github.io"
+
+echo "REMOVING PREVIOUS CONTENT"
+rm -rf `ls | grep -v *.sh | grep -v source | grep -v README.md`
+
+echo "STORING $LOCAL_GHOST content in $OUTDIR folder"
+wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST/ && \
+
+echo "REPLACING $LOCAL_GHOST URL WITH $GITHUB_PAGES_URL"
+grep -r "$LOCAL_GHOST" -l --null . --exclude=*.sh --exclude-dir=source | xargs -0 sed -i "" -e "s#$LOCAL_GHOST#$GITHUB_PAGES_URL#g"
+
+echo "UPDATING GITHUB PAGES"
+git add . && \
+git ci -m 'Update website' &&  \
+git push
