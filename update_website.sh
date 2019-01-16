cd9bbfaf7e99198d260963f40f2a45915f908a03
dwissk <dominik.wisskirchen@ebf.de>
2019-01-16 16:39:07 +0100

    Delete generated website

--- update_website.sh
+++ update_website.sh
@@ -3,13 +3,13 @@ OUTDIR=.
 LOCAL_GHOST="http://localhost:2368"
 GITHUB_PAGES_URL="http://twoandahalfvan.github.io"
 
-echo "STORING $LOCAL_GHOST content in $OUTDIR folder"
-wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST/
-
 echo "DELETING OBSOLETE STATIC CONTENT"
 rm -rf `ls |grep -v README.md |grep -v source | grep -v *.sh`
 git add .
 git commit -m 'Delete generated website'
+
+echo "STORING $LOCAL_GHOST content in $OUTDIR folder"
+wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST/
 
 echo "REPLACING $LOCAL_GHOST URL WITH $GITHUB_PAGES_URL"
 grep -r "$LOCAL_GHOST" -l --null . --exclude=*.sh --exclude-dir=source | xargs -0 sed -i "" -e "s#$LOCAL_GHOST#$GITHUB_PAGES_URL#g"
