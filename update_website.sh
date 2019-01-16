1b9d8074763250d703d227cc020c1ec0e31249d8
Dominik <d.wisskirchen@gmail.com>
2019-01-16 14:43:48 +0100

    do not convert links to local links

--- update_website.sh
+++ update_website.sh
@@ -4,7 +4,7 @@ LOCAL_GHOST="http://localhost:2368"
 GITHUB_PAGES_URL="http://twoandahalfvan.github.io"
 
 echo "STORING $LOCAL_GHOST content in $OUTDIR folder"
-wget -r -nH -P $OUTDIR -E -T 2 -np -k $LOCAL_GHOST/
+wget -r -nH -P $OUTDIR -E -T 2 -np $LOCAL_GHOST/
 
 echo "REPLACING $LOCAL_GHOST URL WITH $GITHUB_PAGES_URL"
 grep -r "$LOCAL_GHOST" -l --null . --exclude=*.sh --exclude-dir=source | xargs -0 sed -i "" -e "s#$LOCAL_GHOST#$GITHUB_PAGES_URL#g"
